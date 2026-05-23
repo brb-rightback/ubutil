7cfeee091680efa4f395922c2c1f8346dd3f1442
Benjamin Bogart <bbogart@uboonegpvm03.fnal.gov>
2026-05-23 13:52:09 -0500

    Scripts for production of runs1/3 DetVars

---
+++ scripts/set_wc_detvar_filetype.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#------------------------------------------------------------------
+#
+# Purpose: This script runs merge_dlreco_fnal_data_lantern.sh. 
+#          but only if the grid was unable to fetch the dlreco file.
+#
+# Created: Ben Bogart (benbogart777@gmail.com@fnal.gov), 23-May-2026 
+#
+#------------------------------------------------------------------
+
+# Read the config
+
+deffiletype=''
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+
+  -t|--filetype )
+    if [ $# -gt 1 ]; then
+      echo "Defining WireCell DetVar filetype to be $2"
+      deffiletype=$2
+      shift
+    else
+      echo "Bad $1 argument"
+      exit 1
+    fi
+    ;;
+
+  -* )
+    echo "Unknown option $1"
+    dohelp
+    exit 1
+    ;;
+
+  * )
+    echo "Bad argument $1"
+    dohelp
+    exit 1
+    ;;
+
+  esac
+  shift
+done
+
+# Check options.
+
+if [ x$reco1def = x ]; then
+  echo "No WireCell DetVar filetype specified."
+  exit 1
+fi
+
+echo $reco1def > $reco1def.fcl
+
+exit 0
